5048998c064c7086f40cba7b013289876133c036
Andre Mattos <andremattos@gmail.com>
2018-05-14 18:41:04 -0300

    adding script collect

---
+++ aula2/PlayerControllerCollect.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControllerCollect : MonoBehaviour {
+
+  public float speed;
+
+  private Rigidbody rb;
+
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  private void FixedUpdate()
+  {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+    rb.AddForce(movement * speed);
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if(other.gameObject.CompareTag("Pick Up")){
+      other.gameObject.SetActive(false);
+    }
+  }
+}
